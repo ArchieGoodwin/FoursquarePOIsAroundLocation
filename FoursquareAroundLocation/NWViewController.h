cc3257333b522c9d9e05d6d89939ac86f0fecf31
Sergey Dikarev <sdikarev@gmail.com>
2013-02-05 14:28:51 +0400

    Initial Commit

---
+++ FoursquareAroundLocation/NWViewController.h
@@ -0,0 +1,13 @@
+//
+//  NWViewController.h
+//  FoursquareAroundLocation
+//
+//  Created by Sergey Dikarev on 2/5/13.
+//  Copyright (c) 2013 Sergey Dikarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWViewController : UIViewController
+
+@end
